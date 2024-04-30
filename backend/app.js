6842111ed569b3b9c72cbc1538374126d9363724
liuyuelintop <liuyuelintop@gmail.com>
2024-04-30 16:09:24 +1000

    Complete SendMessage endpoint and protectRoute

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { config } from "dotenv";
 import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 const app = express();
 config();
 
 //middlewares
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", appRouter);
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
+import messageRoutes from "./message.routes.js";
 
 const appRouter = Router();
 
 appRouter.use("/auth", authRoutes);
+appRouter.use("/messages", messageRoutes);
 
 export default appRouter;
