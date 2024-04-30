7ed10177abf030361d38db18c970e6e7e2a5f030
liuyuelintop <liuyuelintop@gmail.com>
2024-04-30 16:27:16 +1000

    1st Part Completed: backend.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import messageRoutes from "./message.routes.js";
+import userRoutes from "./user.routes.js";
 
 const appRouter = Router();
 
 appRouter.use("/auth", authRoutes);
 appRouter.use("/messages", messageRoutes);
+appRouter.use("/users", userRoutes);
 
 export default appRouter;
