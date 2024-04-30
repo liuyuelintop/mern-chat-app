a469b5dd775d9cdf3883bd749bc934f6919503b6
liuyuelintop <liuyuelintop@gmail.com>
2024-04-30 15:41:30 +1000

    Complete Login and Logout endpoints

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -51,9 +51,34 @@ export const signup = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
-export const login = (req, res) => {
-  res.send("login");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = bcrypt.compare(password, user?.password || "");
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+    const userId = user.id;
+    generateTokenAndSetCookie({ payload: { userId }, res });
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
-export const logout = (req, res) => {
-  res.send("logout");
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
