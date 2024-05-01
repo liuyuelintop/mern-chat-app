6b58f00a2f87b009abac7ec9633e9cbfe4a73e31
liuyuelintop <liuyuelintop@gmail.com>
2024-05-01 17:00:59 +1000

    Complete Get Conversations functionality: useConversation and useGetConversations hooks, Conversation and ConversationCard components

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,22 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import ConversationCard from "./ConversationCard";
 
 const Conversations = () => {
+    const { loading, conversations } = useGetConversations();
+
     return (
         <div className='py-2 flex flex-col overflow-auto'>
-            <ConversationCard />
-            <ConversationCard />
-            <ConversationCard />
-            <ConversationCard />
-            <ConversationCard />
-            <ConversationCard />
+            {conversations.map((conversation, idx) => (
+                <ConversationCard
+                    key={conversation._id}
+                    conversation={conversation}
+                    emoji={getRandomEmoji()}
+                    lastIdx={idx === conversations.length - 1}
+                />
+            ))}
+
+            {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
         </div>
     );
 };
